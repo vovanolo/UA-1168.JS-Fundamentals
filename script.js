0671655271da62d3d20ca052fc669f0167360271
vovanolo <vpankiv@softserveinc.com>
2024-03-11 19:35:16 +0200

    topic - 1

---
+++ script.js
@@ -0,0 +1,58 @@
+// alert("Hello js");
+
+// console.log("Hello js"); //вивід в консоль
+
+//коментар
+
+
+// const let var
+
+// let name = "Volodymyr";
+// const name = "Volodymyr";
+// let number = 5;
+
+// // name = "Ivan";
+// // name = number;
+
+// let text = "Hello im 'Igor'"
+
+// console.log(text);
+
+
+// const user = {
+//     name: "Volodymyr",
+//     surname: "Pankiv",
+//     age: 30,
+// }
+
+// console.log(user);
+
+// let name = "Ivan";
+
+// name = 5;
+
+// console.log(name);
+// console.log(typeof name);
+// console.log(typeof(name));
+
+// Взаємодія з користувачем
+
+// alert() confirm() prompt()
+
+// alert("Hello world!");
+
+// prompt("Please enter your name:");
+
+// let name = prompt("Please enter your name");
+
+// alert("Your name is :" + name);
+// alert(`Your name is ${name}`);
+
+
+// confirm("Are you ok?");
+// let ok = confirm("Are you ok?");
+
+// console.log(ok);
+
+
+
