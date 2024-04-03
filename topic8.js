47b221539a6df7d451123f1859525fe7c188145d
vovanolo <vpankiv@softserveinc.com>
2024-04-03 19:47:09 +0300

    topic-08

---
+++ topic8.js
@@ -0,0 +1,35 @@
+// let regexp = new RegExp()
+
+//     /  регулярний вираз  /
+
+//   /saf2343/
+
+// let string = "Hello guys Hello";
+
+// let regexp = /hello/ - flags ;
+
+
+// let regexp = /hello/i;
+
+// let regexp = /hello/g;
+// let regexp = /hello/m;
+// let regexp = /hello/gi;
+
+// string.match(//);
+
+
+
+
+
+// console.log(regexp.test(string));
+
+
+// let string = "Hello guys Hello";
+
+// console.log(string.match(/Hello/));
+// console.log(string.match(/Hello/g));
+// console.log(string.match(/hello/gi));
+
+// console.log("feefegrg 223224".match(/\d+\.?\d*/));
+
+
